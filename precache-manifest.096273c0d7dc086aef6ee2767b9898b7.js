114ccf7b21aa5cdc82ae7b4253d83e7bef6510fa
MuradIbrahimov <murad.ibrahimov321@gmail.com>
2023-03-05 15:21:06 +0400

    Updates

---
+++ precache-manifest.096273c0d7dc086aef6ee2767b9898b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0d25c0a66c04208bd76",
+    "url": "/expenserecorder/static/css/main.ca37cf2b.chunk.css"
+  },
+  {
+    "revision": "d0d25c0a66c04208bd76",
+    "url": "/expenserecorder/static/js/main.d0d25c0a.chunk.js"
+  },
+  {
+    "revision": "71d1f42601588a34bb98",
+    "url": "/expenserecorder/static/js/1.71d1f426.chunk.js"
+  },
+  {
+    "revision": "d54e53cd2b6285eeb84c",
+    "url": "/expenserecorder/static/js/runtime~main.d54e53cd.js"
+  },
+  {
+    "revision": "3d341a218b1324ddab6cac4923d8710a",
+    "url": "/expenserecorder/index.html"
+  }
+];
